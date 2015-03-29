62c510615fd71ffaf7a357a2cc6d4a31fe060c4a
ekan <ekan2@jhu.edu>
2015-03-28 22:55:39 -0400

    Python script to turn light on

---
+++ lighton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+#from mod_python import apache
+
+def lighton():
+	
+	LED = 11
+
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(LED, GPIO.OUT)
+	GPIO.output(LED, GPIO.LOW)
+	GPIO.output(LED,GPIO.HIGH)
+	
+	print("lighton.py executed!")
+
+lighton()
