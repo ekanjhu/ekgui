c478167d8da41dd546297627218938b0c68635f6
ekan <ekan2@jhu.edu>
2015-04-01 23:37:08 -0400

    Implemented total of 4 buttons: 1 pair of buttons to turn on/off light switch in room1 1 pair of buttons to turn on/off light switch in room2 camera is not working yet

--- index.php
+++ index.php
@@ -6,45 +6,101 @@
 <script> $(document).ready(function(){
 		$("#btnon1").click(function(){
 			event.preventDefault();
+			var button_id = $(this).attr("id");
 			//alert("Text: "+ $("#test").text());	
 			$.ajax({
+				url: "script_test.php",
 				type: "POST",
-				url: "/var/www/lighton.py",
-				success: function(data, textStatus,jqXHR){
-					console.log('It worked');
+				data:{id:button_id},
+				success: function(ajaxresult){
+					$("#ajaxrequest").html(ajaxresult);
+					console.log('It worked1');
 				}
-			})
+			});
 		});
+	
+		 $("#btnoff1").click(function(){
+                        event.preventDefault();
+                        var button_id2 = $(this).attr("id");
+                        $.ajax({
+                                url: "script_test.php",
+                                type: "POST",
+                                data:{id:button_id2},
+                                success: function(ajaxresult){
+                                        $("#ajaxrequest").html(ajaxresult);
+                                        console.log('It worked2');
+                                }
+                        });
+                });
+
+		 $("#btnon2").click(function(){
+                        event.preventDefault();
+                        var button_id3 = $(this).attr("id");
+                        //alert("Text: "+ $("#test").text());   
+                        $.ajax({
+                                url: "script_test.php",
+                                type: "POST",
+                                data:{id:button_id3},
+                                success: function(ajaxresult){
+                                        $("#ajaxrequest").html(ajaxresult);
+                                        console.log('It worked3');
+                                }
+                        });
+                });
+
+                 $("#btnoff2").click(function(){
+                        event.preventDefault();
+                        var button_id4 = $(this).attr("id");
+                        $.ajax({
+                                url: "script_test.php",
+                                type: "POST",
+                                data:{id:button_id4},
+                                success: function(ajaxresult){
+                                        $("#ajaxrequest").html(ajaxresult);
+                                        console.log('It worked4');
+                                }
+                        });
+                });
+
+		
+		$("#start_livevideo").click(function(){
+                        event.preventDefault();
+                        var button_id5 = $(this).attr("id");
+                        $.ajax({
+                                url: "script_test.php",
+                                type: "POST",
+                                data:{id:button_id5},
+                                success: function(ajaxresult){
+                                        $("#ajaxrequest").html(ajaxresult);
+                                        console.log('It worked5');
+                                }
+                        });
+                });
+
+
+
+
 	});
 </script>
 </head>
 <body>
+<div id="ajaxrequest"></div>
 <p id="LightControl1">-------- Room 1--------</p>
-<button id="btnon1"> Turn Light On</button>
+<button id="btnon1"> Turn Light On </button>
 <br></br>
-<button id="btnoff1"> Turn Light Off</button>
-<br></br>
-<p> Table of Light States</p>
+<button id="btnoff1"> Turn Light Off </button>
 
-<p id="LightControl2">--------  Room 2-------- </p>
-<button id="btnon2"> Turn Light On</button>
+<p id="LightControl2">-------- Room 2--------</p>
+<button id="btnon2"> Turn Light On </button>
 <br></br>
-<button id="btnoff2"> Turn Light Off</button>
-<br></br>
-<p> Table of Light States</p>
+<button id="btnoff2"> Turn Light Off </button>
 
-<p id="LiveVideo"> --------Surveillance Camera---------</p>
-<button id ="StreamStart">Start Live Video Stream </button>
+<p id="Live Video Control"> --------Surveillance Camera--------</p>
+<button id="start_livevideo" align="left">  Start Live Video </button>
 <br></br>
-<button id ="StreamStop">Stop Live Video Stream </button>
+<button id="stop_livevideo" align="center"> Stop Live Video </button>
 <br></br>
-<!--<img src="http://169.254.64.100:8080/?action=stream" width=200 height=200>-->
-<!--<br><a href="http://169.254.64.100/?action=stream">View</a>-->
 <embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" ve$
-<br></br>
-<p> Table of Event-Triggered Recordings </p>
-
-
 
 
 </body>
