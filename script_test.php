5ba17351e60f179e9cf4e30c3942b4b42d774c93
ekan <ekan2@jhu.edu>
2015-04-05 02:16:50 -0400

    Added logic to handle slider Added logic to handle button click for activating motion detector Added logic to handle button to "kill" streaming process on RPi (hard stop)

--- script_test.php
+++ script_test.php
@@ -1,5 +1,6 @@
 <?php
 $post_button =$_REQUEST["id"];
+$post_value  =$_REQUEST["angle"];
 if($post_button == "btnon1")
 {
 	echo "Room1: Turned light on"."<br>";
@@ -23,13 +24,21 @@ if($post_button == "btnon1")
 	shell_exec('sudo python /home/pi/lightoff2.py');
 
 }elseif($post_button == "start_livevideo"){
-
 	echo "Starting Live Video"."<br>";
 	$result=shell_exec('/var/www/camloop.sh');
 	echo "<pre> $result </pre>";
-
-}elseif($post_button == "stop_livevideo"){
-	echo "Stopping Live Video"."<br>";
+}elseif($post_button == "kill_raspivid"){
+	echo "Stopping Raspivid"."<br>";
 	$result2=shell_exec('/var/www/camloop_stop.sh');
+}elseif($post_button == "slider"){
+	echo "slider changed"."<br>";
+	echo "user selected angle=".$post_value."<br>";
+	shell_exec('sudo python /home/pi/servo_slidercontrol.py '.$post_value);
+}elseif($post_button == "activate_motiondetector"){
+	echo "turn on motion detector"."<br>";
+	shell_exec('sudo python /home/pi/pir_capture_video_scan.py');
+
 }
+
+
 ?>
