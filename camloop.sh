8af7c4a1dc78a6e37b0b6736b747e91c7421e16d
ekan <ekan2@jhu.edu>
2015-04-22 23:07:43 -0400

    Vertically flips the camera shot so that everything is oriented correctly.

--- camloop.sh
+++ camloop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "camloop called"
-sudo raspivid -o - -t 0 -hf -w 600 -h 400 -fps 25| cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
+sudo raspivid -o - -t 0 -hf -vf -w 600 -h 400 -fps 25| cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
